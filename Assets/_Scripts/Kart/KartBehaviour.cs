240a5b48dbc70635cab790e6d4fc8aca85607053
TuomoO <tuomooksanen@kamk.fi>
2016-01-14 13:55:44 +0200

    Kart moving scripts

---
+++ Assets/_Scripts/Kart/KartBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarBehaviour : MonoBehaviour {
+
+    public float speed;
+    private float maxSpeed;
+    private float engineDeceleration;
+
+	// Use this for initialization
+	void Start () {
+        maxSpeed = 12.5f;
+        speed = 0;
+        engineDeceleration = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //gas
+        speed -= engineDeceleration * Time.deltaTime;
+        speed = Mathf.Min(speed, maxSpeed);
+        speed = Mathf.Max(speed, 0);
+
+        //move
+        transform.position += speed * Time.deltaTime * transform.forward;
+	}
+}
---
+++ Assets/_Scripts/Kart/KartInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carInput : MonoBehaviour {
+
+    private float turnSpeed;
+    private float acceleration;
+
+    private CarBehaviour carScript;
+
+	// Use this for initialization
+	void Start () {
+        turnSpeed = 125;
+        acceleration = 7.5f;
+        carScript = transform.transform.GetComponent<CarBehaviour>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //gas
+        carScript.speed += Input.GetAxis("Vertical") * Time.deltaTime * acceleration;
+
+        //turn
+        transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0));
+	}
+}
