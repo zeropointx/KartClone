bfaea71c96b685d12a9041501730c955ad9bcf49
TuomoOks <tuomooksanen@kamk.fi>
2016-01-15 13:06:00 +0200

    Ajusted Kart steering

--- Assets/_Scripts/Kart/KartBehaviour.cs
+++ Assets/_Scripts/Kart/KartBehaviour.cs
@@ -3,25 +3,68 @@ using System.Collections;
 
 public class KartBehaviour : MonoBehaviour {
 
-    public float speed;
-    public float maxSpeed;
+    private float steeringWheel;
+    private float pedal;
+    private float speed;
+    private float maxSpeed;
+    private float turnSpeed;
+    private float maxTurn;
+    private float acceleration;
+    private float brakeForce;
     private float engineDeceleration;
 
 	// Use this for initialization
 	void Start () {
         maxSpeed = 50;
         speed = 0;
+        turnSpeed = 75;
+        acceleration = 25;
+        brakeForce = 45;
         engineDeceleration = 10f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        float speedChange = 0;
+        float rotation = 0;
+
         //gas
+        if (Mathf.Abs(pedal) > 0)
+            speedChange = (pedal < 0) ? brakeForce : acceleration;
+
+        //steer
+        if (Mathf.Abs(speed) > 0)
+        {
+            rotation = steeringWheel;
+            if (speed < 0.5f * maxSpeed)
+                rotation *= Mathf.Abs(speed) * 0.075f;
+            //speedChange *= 0.75f;
+        }    
+        
+        //speed
         speed -= engineDeceleration * Time.deltaTime;
+        speed += speedChange * pedal * Time.deltaTime;
         speed = Mathf.Min(speed, maxSpeed);
         speed = Mathf.Max(speed, 0);
 
-        //move
+        //transform
+        transform.Rotate(new Vector3(0, turnSpeed * rotation * Time.deltaTime, 0));
         transform.position += speed * Time.deltaTime * transform.forward;
 	}
+
+    public void Accelerate(float pedalValue) {
+        pedal = pedalValue;
+        pedal = Mathf.Min(pedal, 1);
+        pedal = Mathf.Max(pedal, -1);
+    }
+
+    public void Steer(float wheelPosition) {
+        steeringWheel = wheelPosition;
+        steeringWheel = Mathf.Min(steeringWheel, 1);
+        steeringWheel = Mathf.Max(steeringWheel, -1);
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
 }
--- Assets/_Scripts/Kart/KartInput.cs
+++ Assets/_Scripts/Kart/KartInput.cs
@@ -3,35 +3,16 @@ using System.Collections;
 
 public class KartInput : MonoBehaviour {
 
-    private float turnSpeed;
-    private float maxTurn;
-    private float acceleration;
-    private float brakeForce;
     private KartBehaviour kartScript;
 
 	// Use this for initialization
 	void Start () {
-        turnSpeed = 4.5f;
-        acceleration = 25;
-        brakeForce = 45;
-        kartScript = transform.transform.GetComponent<KartBehaviour>();
+        kartScript = transform.GetComponent<KartBehaviour>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //gas
-        float pedal = Input.GetAxis("Vertical");
-        if (pedal < 0)
-            kartScript.speed += pedal * Time.deltaTime * brakeForce;
-        else
-            kartScript.speed += pedal * Time.deltaTime * acceleration;
-
-        //turn
-        float rotation = 0;
-        if (Mathf.Abs(kartScript.speed) > 0)
-        {
-            rotation = Input.GetAxis("Horizontal") * turnSpeed * Mathf.Abs(kartScript.speed)* Time.deltaTime;
-            transform.Rotate(new Vector3(0, rotation, 0));
-        }
+        kartScript.Accelerate(Input.GetAxis("Vertical"));
+        kartScript.Steer(Input.GetAxis("Horizontal"));
 	}
 }
