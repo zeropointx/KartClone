bb517801fd9fa4aa36ebf304ecf39be4431c9af7
Zegram <Joikkiz@hotmail.com>
2016-01-19 20:12:43 +0200

    Fixed weaponImageUI to work with prefabs.

--- Assets/_Scripts/Inventory/InventoryScript.cs
+++ Assets/_Scripts/Inventory/InventoryScript.cs
@@ -12,13 +12,14 @@ public class InventoryScript : MonoBehaviour
     public GameObject weaponImageUI;
     weaponImageScript imageScript;
     public int randomSeed = 1337;
-    
+
 
     void Start()
     {
         Random.seed = randomSeed;
         currentWeapon = WEAPON.noWeapon;
-       imageScript = weaponImageUI.GetComponent<weaponImageScript>();
+        weaponImageUI = GameObject.Find("weaponImageUI");
+        imageScript = weaponImageUI.GetComponent<weaponImageScript>();
     }
 
     void Update()
@@ -46,25 +47,25 @@ public class InventoryScript : MonoBehaviour
 
     public void updateWeaponTexture()
     {
-        if(currentWeapon == WEAPON.noWeapon)
+        if (currentWeapon == WEAPON.noWeapon)
         {
             imageScript.updateSprite(noWeaponSprite);
         }
 
-        else if(currentWeapon == WEAPON.Weapon1)
+        else if (currentWeapon == WEAPON.Weapon1)
         {
             imageScript.updateSprite(weapon1Sprite);
         }
 
-        else if(currentWeapon == WEAPON.Weapon2)
+        else if (currentWeapon == WEAPON.Weapon2)
         {
             imageScript.updateSprite(weapon2Sprite);
         }
 
-        else if(currentWeapon == WEAPON.Weapon3)
+        else if (currentWeapon == WEAPON.Weapon3)
         {
             imageScript.updateSprite(weapon3Sprite);
-        }       
+        }
     }
 
 
