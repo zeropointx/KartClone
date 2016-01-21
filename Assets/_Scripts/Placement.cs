8a894be4ea57118d0c8ab5d79bfb978502c6d55c
Zegram <Joikkiz@hotmail.com>
2016-01-21 13:49:11 +0200

    Laps & checkpoints work now, testing track in kartTest scene.

---
+++ Assets/_Scripts/Placement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Placement : MonoBehaviour
+{
+    public int currentLap = 0;
+    public int currentCheckPointIndex = 0;
+
+    // These are fetched from the track information
+    int checkpointAmount;
+    int lapAmount;
+
+    public List<GameObject> checkPoints = new List<GameObject>();
+    public GameObject track;
+    TrackInformation trackInformation;
+
+    void Start()
+    {
+        trackInformation = track.GetComponent<TrackInformation>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject GG = col.gameObject;
+        if (GG.tag == "checkPoint")
+        {
+            for (int i = 0; i < checkPoints.Count; i++)
+            {
+                if (GG == checkPoints[i])
+                {
+                    if (GG == checkPoints[currentCheckPointIndex + 1])
+                    {
+                        currentCheckPointIndex++;
+                        if (currentCheckPointIndex == (checkPoints.Count - 1))
+                        {
+                            Debug.Log("Doge");
+                            currentLap++;
+                            currentCheckPointIndex = 0;
+
+                            if (currentLap == trackInformation.lapAmount)
+                            {
+                                //Wonnered
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/TrackInformation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrackInformation : MonoBehaviour
+{
+
+    public int lapAmount;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+   
+}
