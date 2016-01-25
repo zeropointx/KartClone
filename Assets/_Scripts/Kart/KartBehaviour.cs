33befa7071a06d92c176a36b1768c19d54050387
TuomoO <tuomooksanen@kamk.fi>
2016-01-25 12:34:07 +0200

    Fixed kart acceleration, added reverse gear

--- Assets/_Scripts/Kart/KartBehaviour.cs
+++ Assets/_Scripts/Kart/KartBehaviour.cs
@@ -25,7 +25,6 @@ public class KartBehaviour : MonoBehaviour {
     KartState state;
     float collisionImmunityTimer;
     float stopTimer;
-    //Rigidbody rigidbody;
 
 	// Use this for initialization
 	void Start () {
@@ -33,14 +32,13 @@ public class KartBehaviour : MonoBehaviour {
         pedal = 0;
         speed = 0;
         maxSpeed = 45;
-        maxReverse = 15;
+        maxReverse = -15;
         turnSpeed = 75;
         acceleration = 0.35f;
         brakeForce = 1.25f;
         engineDeceleration = 0.15f;
         groundDistance = 0;
         mainCamera = transform.FindChild("Main Camera").gameObject;
-        //rigidbody = this.FindComponent("Rigidbody");
         tiltLimitX = 30;
         tiltLimitZ = 45;
         collisionImmunityTimer = 0;
@@ -52,83 +50,59 @@ public class KartBehaviour : MonoBehaviour {
 	void Update () {
         //timers
         collisionImmunityTimer += Time.deltaTime;
-        stopTimer += Time.deltaTime;
 
         float speedChange = 0;
-        Vector3 direction = transform.forward;
 
         //controls
-        if (stopTimer > 1)
+        switch (state)
         {
-            switch (state)
-            {
-                case KartState.FORWARD:
-                    //Debug.Log("for");
-                    if (pedal != 0)
-                        speedChange = (pedal > 0) ? acceleration : brakeForce;
-                    UpdateControls();
-                    if (groundDistance < 3)
-                    {
-                        speed -= engineDeceleration;
-                        speed += speedChange * pedal;
-                    }
-                    Mathf.Clamp(speed, 0, maxSpeed);
-                    speed = Mathf.Max(speed, 0);
-                    if (speed == 0)
-                        state = KartState.STOPPED;
-                    break;
+            case KartState.FORWARD:
+                if (pedal != 0)
+                    speedChange = (pedal > 0) ? acceleration : brakeForce;
+                UpdateControls();
+                if (groundDistance < 3)
+                {
+                    speed -= engineDeceleration;
+                    speed += speedChange * pedal;
+                }
+                speed = Mathf.Clamp(speed, 0, maxSpeed);
+                if (speed == 0)
+                    state = KartState.STOPPED;
+                break;
 
-                case KartState.STOPPED:
-                    //Debug.Log("stop");
-                    speed = 0;
-
+            case KartState.STOPPED:
+                stopTimer += Time.deltaTime;
+                speed = 0;
+                if (stopTimer > 0.25f)
+                {
                     if (pedal != 0)
                     {
                         state = (pedal > 0) ? KartState.FORWARD : KartState.REVERSE;
-                        //stopTimer = 0;
+                        stopTimer = 0;
                     }
-                    break;
+                }
+                break;
 
-                case KartState.REVERSE:
-                    /*
-                    Debug.Log("rev");
-                    if (pedal != 0)
-                        speedChange = (pedal > 0) ? -0.1f * brakeForce : -0.1f * acceleration;
-                    speed -= engineDeceleration;
-                    direction *= -1.0f;
-                    UpdateControls();
+            case KartState.REVERSE:
+                if (pedal != 0)
+                    speedChange = (pedal > 0) ? brakeForce : acceleration;
+                UpdateControls();
+                if (groundDistance < 3)
+                {
+                    speed += engineDeceleration;
                     speed += speedChange * pedal;
-                    Mathf.Clamp(speed, -maxReverse, 0);
-                    if (speed == 0)
-                        state = KartState.STOPPED;
-                    */
+                }
+                speed = Mathf.Clamp(speed, maxReverse, 0);
+                if (speed == 0)
                     state = KartState.STOPPED;
-                    break;
+                break;
 
-                default:
-                    Debug.LogError("invalid Kart state!");
-                    break;
-            }
+            default:
+                Debug.LogError("invalid Kart state!");
+                break;
         }
-        /*
-        if (Mathf.Abs(pedal) > 0)
-            speedChange = (pedal < 0) ? brakeForce : acceleration;
-        */
 
-        //transform
-        //speed -= engineDeceleration;
-        /*
-        if (groundDistance < 5)
-        {
-            speed += speedChange * Mathf.Abs(pedal);
-            if (Mathf.Abs(steer) > 0)
-                transform.Rotate(new Vector3(0, turnSpeed * steer * Time.deltaTime, 0));
-        }
-        
-        speed = Mathf.Min(speed, maxSpeed);
-        speed = Mathf.Max(speed, 0);
         Vector3 direction = transform.forward;
-        */
         if (groundDistance > 3)
             direction -= groundNormal * 0.25f;
         transform.position += speed * Time.deltaTime * direction;
