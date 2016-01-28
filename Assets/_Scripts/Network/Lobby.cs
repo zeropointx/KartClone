b0963bfe29452209aa94ad863186b875a3017cc3
havyton <tti13smiikas@kamk.fi>
2016-01-28 15:23:24 +0200

    Started adding lobby

---
+++ Assets/_Scripts/Network/Lobby.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Lobby : MonoBehaviour {
+
+	// Use this for initialization
+    GameObject lobby;
+	void Start () {
+        lobby = GameObject.Find("Lobby");
+        lobby.GetComponent<NetworkLobbyManager>().StartServer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
