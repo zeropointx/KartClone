c89e0e75ee64ee6a23377f785899398af7e2bde2
havyton <tti13smiikas@kamk.fi>
2016-01-28 11:55:22 +0200

    Added placement printerino

--- Assets/_Scripts/Gamemode/Gamemode.cs
+++ Assets/_Scripts/Gamemode/Gamemode.cs
@@ -8,6 +8,7 @@ public class Gamemode : NetworkBehaviour {
     public GameObject startTimerText;
     public GameObject statusText;
     private List<Player> players = new List<Player>();
+    public int playerCount = 0;
     public enum State
     {
         WAITING_FOR_PLAYERS,
@@ -17,12 +18,27 @@ public class Gamemode : NetworkBehaviour {
     }
     public struct Player
     {
-        int placement;
-        GameObject gameObject;
+        public int placement;
+        public GameObject gameObject;
         public Player(int placement, GameObject gameObject)
         {
             this.placement = placement;
             this.gameObject = gameObject;
+        }
+    }
+    private struct DebugPlayer
+    {
+        
+        public GameObject gameObject;
+        public int currentLap;
+        public int currentCheckpointIndex;
+        public float distanceToNextCheckpoint;
+        public DebugPlayer(GameObject gameObject,int currentLap,int currentCheckpointIndex, float distanceToNextCheckpoint)
+        {
+            this.gameObject = gameObject;
+            this.currentLap = currentLap;
+            this.currentCheckpointIndex = currentCheckpointIndex;
+            this.distanceToNextCheckpoint = distanceToNextCheckpoint;
         }
     }
 
@@ -30,6 +46,7 @@ public class Gamemode : NetworkBehaviour {
     {
         if (!players.Contains(p))
             players.Add(p);
+        playerCount++;
     }
     [SyncVar]
     public State currentState;
@@ -60,8 +77,60 @@ public class Gamemode : NetworkBehaviour {
                 }
             case State.RACING:
                 {
-                  
-                    break;
+                    List<DebugPlayer> tempPlayerList = new List<DebugPlayer>();
+                    for (int i = 0; i < players.Count; i++ )
+                    {
+                        DebugPlayer dp = new DebugPlayer();
+                        Player p = players[i];
+                        Placement placement = p.gameObject.GetComponent<Placement>();
+                        TrackInformation track = GameObject.Find("Track").GetComponent<TrackInformation>();
+
+                        dp.gameObject = p.gameObject;
+                        dp.currentLap = placement.currentLap;
+                        dp.currentCheckpointIndex = placement.currentCheckPointIndex;
+                        dp.distanceToNextCheckpoint = Vector3.Distance( track.checkPoints[dp.currentCheckpointIndex + 1].transform.position,dp.gameObject.transform.position);
+                        tempPlayerList.Add(dp);
+                    }
+                    players.Clear();
+                   for(int j = 0; j < tempPlayerList.Count; j++)
+                   {
+                        DebugPlayer currentBest = new DebugPlayer();
+                        currentBest.gameObject = null;
+                        for (int i = 0; i < tempPlayerList.Count; i++)
+                        {
+                            DebugPlayer tempPlayer = tempPlayerList[i];
+                            if (containsPlayer(tempPlayer.gameObject))
+                                continue;
+                            if(currentBest.gameObject != null)
+                            {
+                                if(currentBest.currentLap == tempPlayer.currentLap)
+                                {
+                                    
+                                        
+                                    if(currentBest.currentCheckpointIndex == tempPlayer.currentCheckpointIndex)
+                                    {
+                                         if(currentBest.distanceToNextCheckpoint > tempPlayer.distanceToNextCheckpoint)
+                                        {
+                                            currentBest = tempPlayer;
+                                        }
+                                    }
+                                    else if (currentBest.currentCheckpointIndex < tempPlayer.currentCheckpointIndex)
+                                        currentBest = tempPlayer;
+                                }
+                                else if (currentBest.currentLap < tempPlayer.currentLap)
+                                    currentBest = tempPlayer;
+                            }
+                            else
+                            {
+                                currentBest = tempPlayer;
+                            }
+                        }
+
+                       Player p = new Player(players.Count+1,currentBest.gameObject);
+                       players.Add(p);
+
+                    }
+                        break;
                 }
             case State.DONE_RACING:
                 {
@@ -69,6 +138,24 @@ public class Gamemode : NetworkBehaviour {
                 }
         }
 	}
+    public int getPlacement(GameObject g)
+    {
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (players[i].gameObject == g)
+                return players[i].placement;
+        }
+        return -1;
+    }
+    bool containsPlayer(GameObject g)
+    {
+        for(int i = 0; i < players.Count; i++)
+        {
+            if (players[i].gameObject == g)
+                return true;
+        }
+        return false;
+    }
     [ClientRpc]
     void RpcenableInput()
     {
--- Assets/_Scripts/UI/HUD.cs
+++ Assets/_Scripts/UI/HUD.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour {
+    GameObject lapText;
        public GameObject localPlayer = null;
      GameObject weaponImageUI;
+    GameObject track;
+    GameObject placementText;
      weaponImageScript imageScript;
+     public Gamemode gamemode;
      public Sprite weapon1Sprite, weapon2Sprite, weapon3Sprite, noWeaponSprite;
      InventoryScript.WEAPON uiweapon = InventoryScript.WEAPON.noWeapon;
 	// Use this for initialization
 	void Start () {
         weaponImageUI = transform.FindChild("weaponImageUI").gameObject;
         imageScript = weaponImageUI.GetComponent<weaponImageScript>();
+        lapText = GameObject.Find("LapText");
+        track = GameObject.Find("Track");
+        placementText = transform.FindChild("Placement").gameObject;
 	}
 	
 	// Update is called once per frame
@@ -23,7 +31,8 @@ public class HUD : MonoBehaviour {
          InventoryScript.WEAPON currentWeapon = localPlayer.GetComponent<InventoryScript>().currentWeapon;
         if(currentWeapon != uiweapon)
             updateWeaponTexture(currentWeapon);
-
+        lapText.GetComponent<Text>().text = "Lap: " + localPlayer.GetComponent<Placement>().currentLap + "\\" + track.GetComponent<TrackInformation>().lapAmount;
+        placementText.GetComponent<Text>().text = gamemode.getPlacement(localPlayer) + " th";
 	}
 
     public void updateWeaponTexture(InventoryScript.WEAPON currentWeapon)
