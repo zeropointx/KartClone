d96b651d14dbc896dac46fac33c5827c2107ca57
havyton <tti13smiikas@kamk.fi>
2016-02-04 10:01:44 +0200

    Lobby maybe somewhat works

--- Assets/_Scripts/Network/Lobby.cs
+++ Assets/_Scripts/Network/Lobby.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Lobby : MonoBehaviour {
 
 	// Use this for initialization
     GameObject lobby;
+    Text inputField;
 	void Start () {
         lobby = GameObject.Find("Lobby");
-
+        inputField = GameObject.Find("InputField").transform.FindChild("Text").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
@@ -18,5 +20,22 @@ public class Lobby : MonoBehaviour {
     public void StartHost()
     {
         lobby.GetComponent<NetworkLobbyManager>().StartHost();
+    }
+    public void Connect()
+    {
+                if(inputField == null)
+        {
+            Debug.Log("Add inputfield to UILogic script!");
+            return;
+        }
+        string ip = inputField.text;
+        if(ip == null || ip == "" || ip.Length > 20)
+        {
+            Debug.Log("Invalid ip!");
+            return;
+        }
+        lobby.GetComponent<NetworkLobbyManager>().networkAddress = ip;
+        lobby.GetComponent<NetworkLobbyManager>().StartClient();
+        
     }
 }
