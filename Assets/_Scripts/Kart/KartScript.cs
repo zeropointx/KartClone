c1decca1b9d8706ff6eb4f8c7236c92734af3ab9
Zegram <Joikkiz@hotmail.com>
2016-02-18 15:35:30 +0200

    Started on player changing

---
+++ Assets/_Scripts/Kart/KartScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KartScript: MonoBehaviour
+{
+    KartBehaviour KB;
+    public GameObject kartModel;
+    //public float kartMaxSpeed;
+    //public float kartAcceleration;
+    //public float kartTurnSpeed;
+
+	void Start () 
+    {
+        KB = gameObject.GetComponent<KartBehaviour>();
+        kartModel = transform.Find("Kart").gameObject;
+	}
+	
+	void Update () 
+    {
+	
+	}
+
+    public void updatePlayerComponents(GameObject Model, float MaxSpeed, float Acceleration, float TurnSpeed)
+    {
+        kartModel = Model;
+        KB.maxSpeed = MaxSpeed;
+        KB.acceleration = Acceleration;
+        KB.turnSpeed = TurnSpeed;
+    }
+}
