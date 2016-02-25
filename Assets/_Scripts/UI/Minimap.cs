190bd7dc0579e5652c06b25f4e6f547ee9d41569
TuomoO <tuomooksanen@kamk.fi>
2016-02-25 10:00:42 +0200

    Added minimap for local player

---
+++ Assets/_Scripts/UI/Minimap.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Minimap : MonoBehaviour {
+
+    private Vector3 middle;
+    private GameObject marker;
+    private GameObject player;
+    private const float coordinateScale = 0.1f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        middle = GameObject.Find("track").transform.position;
+        marker = transform.FindChild("MinimapMarker").gameObject;
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 dist = (player.transform.position - middle) * coordinateScale;
+        marker.GetComponent<RectTransform>().anchoredPosition = new Vector3(dist.x, dist.z, 0);
+    }
+}
