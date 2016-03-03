13464b12e9d9ec93d01d652f142176247465ab2d
Zegram <Joikkiz@hotmail.com>
2016-03-03 11:50:49 +0200

    BowlingBall fix for millionth time

--- Assets/_Scripts/Inventory/Weapons/BowlingBall.cs
+++ Assets/_Scripts/Inventory/Weapons/BowlingBall.cs
@@ -4,28 +4,24 @@ using UnityEngine.Networking;
 
 public class BowlingBall : NetworkBehaviour
 {
-    public float thrust = 1;
+    public float thrust = 10000;
+
     public Rigidbody rb;
+    Vector3 oldVel;
+
     float destroyTimer = 0.0f;
     PlayerNetwork PN;
     // Use this for initialization
     void Start()
     {
-        //   if (!isServer)
-        //   {
-
-        //       this.enabled = false;
-        //       return;
-        //      }
-
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(transform.forward * thrust, ForceMode.Impulse);
+        rb.AddForce(transform.forward * thrust, ForceMode.Force);
     }
-
-
 
     void FixedUpdate()
     {
+        oldVel = rb.velocity;
+
         //  if (isServer)
         //   {
         
@@ -45,7 +41,14 @@ public class BowlingBall : NetworkBehaviour
 
     void OnCollisionEnter(Collision col)
     {
+        ContactPoint cp = col.contacts[0];
         GameObject GG = col.gameObject;
+        rb.velocity = Vector3.Reflect(oldVel, cp.normal);
+        rb.velocity += cp.normal * 2.0f;
+        Vector3 tempVel = rb.velocity;
+        tempVel.y = 0.0f;
+        rb.velocity = tempVel;
+
         if(GG.tag == "Player")
         {
             PN = GG.GetComponent<PlayerNetwork>();
