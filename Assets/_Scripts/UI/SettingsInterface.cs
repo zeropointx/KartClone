27a225192cf7ec9efa3f087270d93e2e49c93aa0
havyton <tti13smiikas@kamk.fi>
2016-04-07 16:19:22 +0300

    Added borderless window heheh

--- Assets/_Scripts/UI/SettingsInterface.cs
+++ Assets/_Scripts/UI/SettingsInterface.cs
@@ -2,15 +2,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System.Runtime.InteropServices;
+using System;
 
 public class SettingsInterface : MonoBehaviour {
     public static GameObject settingsMenu = null;
     public Dropdown qualitySettings = null;
     public Dropdown resolutionSettings = null;
+    bool fullscreen = false;
+    Text debugText = null;
+    Dropdown windowSettings = null;
+    /*
+     * BORDERLESS
+     */
+    [DllImport("user32.dll")]
+    static extern IntPtr SetWindowLong(IntPtr hwnd, int _nIndex, int dwNewLong);
+    [DllImport("user32.dll")]
+    static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
+
+    const uint SWP_SHOWWINDOW = 0x0040;
+    const int GWL_STYLE = -16;
+    const int WS_BORDER = 1;
+
+
+    /*
+     * /BORDERLESS
+     * 
+     */
+
+
 	// Use this for initialization
 	void Start () {
         qualitySettings = transform.Find("QualitySettings").GetComponent<Dropdown>();
         resolutionSettings = transform.Find("Resolutions").GetComponent<Dropdown>();
+        debugText = transform.Find("DEBUG").GetComponent<Text>();
+        windowSettings = transform.Find("WindowSettings").GetComponent<Dropdown>();
         SetupInterface();
 	}
 	
@@ -50,15 +78,57 @@ public class SettingsInterface : MonoBehaviour {
         for (int i = 0; i < resolutions.Length; i++ )
         {
             resolutionStrings.Add(resolutions[i].width + " x " + resolutions[i].height);
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            if (Screen.width == resolutions[i].width && Screen.height == resolutions[i].height)
                 resolutionIndex = i;
         }
         resolutionSettings.AddOptions(resolutionStrings);
         resolutionSettings.value = resolutionIndex;
+        debugText.text = Screen.width + ":" + Screen.height; 
     }
     public void ApplySettings()
     {
-        QualitySettings.SetQualityLevel(qualitySettings.value);
+        QualitySettings.SetQualityLevel(qualitySettings.value, false);
+
+        Resolution selectedResolution = Screen.resolutions[resolutionSettings.value];
+   
+
+        switch(windowSettings.options[windowSettings.value].text)
+        {
+            case "Windowed":
+                {
+                    fullscreen = false;
+                    break;
+                }
+            case "Borderless":
+                {
+                    fullscreen = false;
+                    //No boderless for unity editor
+                    if (Application.isEditor)
+                        return;
+                    SetWindowLong(GetForegroundWindow(), GWL_STYLE, WS_BORDER);
+                    bool result = SetWindowPos(
+                        GetForegroundWindow(), 
+                        0,
+                        0,//X
+                        0,//Y
+                        selectedResolution.width,
+                        selectedResolution.height, 
+                        SWP_SHOWWINDOW);
+                   
+                    break;
+                }
+            case "Fullscreen":
+                {
+                    fullscreen = true;
+
+
+                    break;
+                }
+            default:
+                break;
+        }
+        Screen.SetResolution(selectedResolution.width, selectedResolution.height, fullscreen);
+
         Back();
     }
     public void Back()
