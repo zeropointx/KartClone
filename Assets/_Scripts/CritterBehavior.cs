06951629b1fde3ed14779562c20bbab9ad61abff
Zegram <Joikkiz@hotmail.com>
2016-04-29 13:23:41 +0300

    Rat behavior + spawner.

---
+++ Assets/_Scripts/CritterBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CritterBehavior : MonoBehaviour {
+
+    GameObject Destination;
+    Vector3 RandomVec3;
+    public float speed;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Destination = GameObject.Find("RatDestinationPipe");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, Destination.transform.position, step);
+
+        //Vector3 targetDir = Destination.transform.position - transform.position;
+        //Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0F);
+        //transform.rotation = Quaternion.LookRotation(newDir);
+
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == Destination)
+            Destroy(gameObject);
+
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/CritterSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CritterSpawner : MonoBehaviour {
+
+    public GameObject critter;
+    public float spawnTimer;
+    public float time;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        time += Time.deltaTime;
+        if(time >= spawnTimer)
+        {
+            time = 0;
+            Instantiate(critter, transform.position - new Vector3(20, 0, 0), critter.transform.rotation);
+            
+        }
+	}
+}
