e33f4337e9de5cd355a6a8bc0417ae99b8261f12
havyton <tti13smiikas@kamk.fi>
2016-04-29 11:47:06 +0300

    Lobby player authority works with this script.

---
+++ Assets/_Scripts/Network/LobbyPlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class LobbyPlayerScript : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (hasAuthority)
+            CmdTestCommand();
+	}
+	[Command]
+    void CmdTestCommand()
+    {
+        Debug.Log("HEHHEH");
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
