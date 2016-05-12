af600f694206bfd9e2b5b9070ebbe8aaa3d68550
havyton <tti13smiikas@kamk.fi>
2016-05-12 10:21:41 +0300

    added sewermap cross section indicator

---
+++ Assets/_Scripts/SewerCrossing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SewerCrossing : MonoBehaviour {
+    public GameObject north;
+    public GameObject west;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = PlayerNetwork.localPlayer;
+        if (player == null)
+            return;
+        int checkpoint = player.GetComponent<Placement>().currentCheckPointIndex;
+        if(checkpoint <=3)
+        {
+            north.SetActive(true);
+            west.SetActive(false);
+        }
+        else
+        {
+            north.SetActive(false);
+            west.SetActive(true);
+        }
+	}
+}
