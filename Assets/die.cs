f8e755970a4a4754bfe2492df9e7d655d9c088f7
Rambopappa <kari92_92@hotmail.com>
2016-05-12 12:11:05 +0300

    Added gib explosion prefab

---
+++ Assets/die.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class die : MonoBehaviour {
+    public GameObject[] parts;
+    public GameObject explosionPos;
+	// Use this for initialization
+    float power = 1000.0f;
+    float radius = 100.0f;
+    float upwardsForce = 0.5f;
+	void Start () {
+        Explode();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void Explode()
+    {
+        foreach (GameObject ob in parts)
+        {
+            Rigidbody rb = ob.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos.transform.position, radius, upwardsForce);
+
+        }
+    }
+}
