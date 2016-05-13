2df91c467643b8397d69074e0094261d1027531e
Rambopappa <kari92_92@hotmail.com>
2016-05-13 12:14:34 +0300

    Added grass

---
+++ Assets/grass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlayerNetwork.localPlayer != null)
+            transform.LookAt(PlayerNetwork.localPlayer.transform.Find("Main Camera").position);
+        transform.eulerAngles = new Vector3(-90, transform.eulerAngles.y+180, 0);
+	}
+}
